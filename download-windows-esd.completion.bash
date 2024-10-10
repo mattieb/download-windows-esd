5938701a190d0bd5abaa25f134bbb9881c466a42
x <>
2024-10-10 08:34:08 +0300

    improve scripts

--- download-windows-esd.completion.bash
+++ download-windows-esd.completion.bash
@@ -1,43 +1,40 @@
 #!/usr/bin/env bash
 
+set -Eeuo pipefail -o posix
+
 _download_windows_esd_completions() {
-    script="${COMP_WORDS[0]}"
+  script="${COMP_WORDS[0]}"
 
-    case "${COMP_CWORD}" in
-    1)
-        COMPREPLY=($(compgen -W "download shasum url languages architectures editions" "${COMP_WORDS[1]}"))
-        ;;
+  case "${COMP_CWORD}" in
+  1)
+    mapfile -t COMPREPLY < <(compgen -W "download shasum url languages architectures editions" -- "${COMP_WORDS[1]}")
+    ;;
 
-    2)
-        case "${COMP_WORDS[1]}" in
-        download|shasum|url|editions|architectures)
-            IFS=$'\n'
-            COMPREPLY=($(compgen -C "${script}"\ languages "${COMP_WORDS[2]}"))
-            ;;
-        esac
-        ;;
-
-    3)
-        case "${COMP_WORDS[1]}" in
-        download|shasum|url|architectures)
-            IFS=$'\n'
-            COMPREPLY=($(compgen -C "${script}"\ editions\ "${COMP_WORDS[2]}" "${COMP_WORDS[3]}")) 
-            ;;
-        esac
-        ;;
-
-
-    4)
-        case "${COMP_WORDS[1]}" in
-        download|shasum|url)
-            IFS=$'\n'
-            COMPREPLY=($(compgen -C "${script}"\ architectures\ "${COMP_WORDS[2]}"\ "${COMP_WORDS[3]}" "${COMP_WORDS[4]}")) 
-            ;;
-        esac
-        ;;
-
+  2)
+    case "${COMP_WORDS[1]}" in
+    download | shasum | url | editions | architectures)
+      mapfile -t COMPREPLY < <(compgen -W "$(command -v "${script}") languages" -- "${COMP_WORDS[2]}")
+      ;;
     esac
+    ;;
+
+  3)
+    case "${COMP_WORDS[1]}" in
+    download | shasum | url | architectures)
+      mapfile -t COMPREPLY < <(compgen -W "$(command -v "${script}") editions ${COMP_WORDS[2]}" -- "${COMP_WORDS[3]}")
+      ;;
+    esac
+    ;;
+
+  4)
+    case "${COMP_WORDS[1]}" in
+    download | shasum | url)
+      mapfile -t COMPREPLY < <(compgen -W "$(command -v "${script}") architectures" -- "${COMP_WORDS[2]}" "${COMP_WORDS[3]}" "${COMP_WORDS[4]}")
+      ;;
+    esac
+    ;;
+
+  esac
 }
 
 complete -F _download_windows_esd_completions download-windows-esd
-
